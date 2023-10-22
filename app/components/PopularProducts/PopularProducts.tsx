718d35b74113dc2c5a84bdb6e836430d4b723f99
tadeo <tadeo@tadeos-MacBook-Pro.local>
2023-10-22 19:38:09 -0400

    pop

--- app/components/PopularProducts/PopularProducts.tsx
+++ app/components/PopularProducts/PopularProducts.tsx
@@ -5,12 +5,8 @@ import { MeasureMemoryMode } from "vm";
 
 export async function PopularProducts() {
   const totalPrice= (price:number, perc: number): number =>{
-      console.log("price",price)
-      console.log("perc",perc)
-      console.log("resul",perc > 0?
-      (price*(perc/100)).toFixed(2) :price)
-      return  
-        perc > 0?
+    return 
+    perc > 0?
           (price*(perc/100)).toFixed(2) :price
 
   }
@@ -90,7 +86,7 @@ export async function PopularProducts() {
                         </div>
                           <div className=" flex justify-center items-center  w-10 h-10  leading-9 rounded-full bg-green-next-50 ">                        
                             <svg  xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21" fill="none">
-                              <path d="M6.66667 8.83333H4.16667L2.5 18H17.5L15.8333 8.83333H13.3333M6.66667 8.83333V6.33333C6.66667 4.49239 8.15905 3 10 3V3C11.8409 3 13.3333 4.49238 13.3333 6.33333V8.83333M6.66667 8.83333H13.3333M6.66667 8.83333V11.3333M13.3333 8.83333V11.3333" stroke="#1A1A1A" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round"/>
+                              <path d="M6.66667 8.83333H4.16667L2.5 18H17.5L15.8333 8.83333H13.3333M6.66667 8.83333V6.33333C6.66667 4.49239 8.15905 3 10 3V3C11.8409 3 13.3333 4.49238 13.3333 6.33333V8.83333M6.66667 8.83333H13.3333M6.66667 8.83333V11.3333M13.3333 8.83333V11.3333" stroke="#1A1A1A" strokeWidth="1.3" strokeLinecap="round" strokeLinejoin="round"/>
                             </svg>                         
                           </div>
                       </div>
--- app/components/belowHero/BelowHero.tsx
+++ app/components/belowHero/BelowHero.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const BelowHero = () => {
   return (
-    <div className=' container w-[--template-max-width] mx-auto px-auto mt-6 h-max  font-poppins rounded-lg shadow-md'>
+    <div className=' container '>
         Customer
     </div>
   )
