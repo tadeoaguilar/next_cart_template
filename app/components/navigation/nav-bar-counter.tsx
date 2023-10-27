3e5352e86e5c2ea1fb418b38d8abf099eb05cfcd
Tadeo Aguilar <Tadeo.Aguilar@FINNING.COM>
2023-10-27 18:13:56 -0400

    redux 02

--- app/components/navigation/nav-bar-counter.tsx
+++ app/components/navigation/nav-bar-counter.tsx
@@ -8,7 +8,7 @@ export const NavBarCounter = () => {
     <>
         {
             count?
-            <label className=" border border-white-next border-solid  absolute top-0 right-0  bg-[#2C742F] text-[10px] font-medium font-poppins  text-white-next text-center rounded-[50%]  h-4 w-4">
+            <label className=" border border-white-next border-solid  absolute top-0 right-0  bg-[#2C742F] text-[9px] font-medium font-poppins pt-[2px]  text-white-next text-center rounded-full  h-5 w-5">
             {count}
           </label>
             :null    
